09c54f9c20a47d1cb2652d2c9b683f1e7576085d
hisazakura <luthfanarrayyan@gmail.com>
2022-12-20 19:02:18 +0700

    Add project files.

---
+++ WPFTest/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace WPFTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Todo> TodoItems = new ObservableCollection<Todo>(Todo.LoadMockData());
+        public MainWindow()
+        {
+            InitializeComponent();
+            TodoGrid.Items.Clear();
+            TodoGrid.ItemsSource = TodoItems;
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (deadlineTime.Value == null) return;
+            Todo todo = new Todo(titleTextBox.Text, deadlineTime.Value ?? DateTime.Now, descriptionTextBox.Text);
+            TodoItems.Add(todo);
+            titleTextBox.Text = "";
+            deadlineTime.Value = null;
+            descriptionTextBox.Text = "";
+
+        }
+
+        private void todoList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TodoGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TodoGrid_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            DataGridPopup.IsOpen = true;
+        }
+
+        private void DataGridRow_MouseEnter(object sender, MouseEventArgs e)
+        {
+
+            ToolTip tooltip = new ToolTip { Content = "Test" };
+        }
+
+        private void Remove_Click(object sender, RoutedEventArgs e)
+        {
+            Todo todo = ((FrameworkElement)sender).DataContext as Todo;
+            TodoItems.Remove(todo);
+        }
+        private void TodoGrid_AutoGenerate(object sender, EventArgs e)
+        {
+            DataGrid grid = (DataGrid)sender;
+            foreach (var item in grid.Columns)
+            {
+                switch (item.Header.ToString())
+                {
+                    case "Remove":
+                        item.DisplayIndex = grid.Columns.Count - 1;
+                        break;
+                    case "Completed":
+                        item.Header = "";
+                        item.MaxWidth = 20;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ WPFTest/Todo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPFTest
+{
+    internal class Todo
+    {
+        public bool Completed { get; set; }
+        public string Title { get; set; }
+        public DateTime Deadline { get; set; }
+        public string Description { get; set; }
+        
+        public Todo(string title, DateTime deadline, string desc)
+        {
+            this.Title = title;
+            this.Deadline = deadline;
+            this.Description = desc;
+            this.Completed = false;
+        }
+        public static List<Todo> LoadMockData()
+        {
+            List<Todo> MockData = new List<Todo>();
+            MockData.Add(new Todo("Tugas PBO", new DateTime(2022, 12, 23), "Menyelesaikan Tugas PBO"));
+            MockData.Add(new Todo("Natalan", new DateTime(2022, 12, 25), "Menikmati Natalan dan diskon Steam"));
+            MockData.Add(new Todo(
+                "Tes Deskripsi Panjang",
+                DateTime.Now,
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."));
+            return MockData;
+        }
+
+    }
+
+}
